4e351a1029d6a59bc6d0460d49d6cb98405993cd
leandrodevel <leandro.wdsn@gmail.com>
2026-04-12 15:53:35 -0300

    jason e incone

--- sw.js
+++ sw.js
@@ -1,25 +1,16 @@
 
 // sw.js
-self.addEventListener('push', function(event) {
-  const data = event.data ? event.data.json() : { title: 'Nova Oferta!', body: 'Confira as promoções.' };
+const cacheName = 'v1';
+const resourcesToPrecache = ['index.html', 'style.css', 'app.js'];
 
-  const options = {
-    body: data.body,
-    icon: './img/icon.png', // Ícone da sua loja
-    badge: './img/badge.png', // Ícone pequeno na barra de status
-    vibrate: [100, 50, 100],
-    data: { url: 'https://seusite.com' } // URL para abrir ao clicar
-  };
-
-  event.waitUntil(
-    self.registration.showNotification(data.title, options)
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(resourcesToPrecache))
   );
 });
 
-// Ação ao clicar na notificação
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow(event.notification.data.url)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
